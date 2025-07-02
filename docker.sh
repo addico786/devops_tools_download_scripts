1c7240949e0e21e218cb98c5235908fd240fbbe5
addico786 <adnandelhi2004@gmail.com>
2025-07-02 19:54:18 +0000

    initial commit

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
-sudo apt-get install docker.io
+sudo apt-get install docker.io -y
 echo "######################################################## now restart your terminal and then run docker ps ########################################################"
 sudo usermod -aG docker $USER && newgrp
--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 echo "################################## installing JAVA ##################################"
 sudo apt update
-sudo apt install fontconfig openjdk-21-jre
+sudo apt install fontconfig openjdk-21-jre -y
 java -version
 echo "################################## JAVA installed succesfully ##################################"
 
@@ -11,7 +11,11 @@ echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 echo "################################## jenkins installed sucessfully ##################################"
+
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
 echo "the above is your temporary password to login to jenkins"
+
+echo "open localhost:8080 in your browser and paste the temporary password"
