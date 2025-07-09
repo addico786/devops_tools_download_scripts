690c976bb49cd4d3132e8a37af60c1b02ebab61c
addico786 <adnandelhi2004@gmail.com>
2025-07-09 12:05:01 +0000

    initial commit

--- terraform.sh
+++ terraform.sh
@@ -1,12 +1,22 @@
+#!/bin/bash
+
 echo "======================================="
 echo "#################### installing Terraform ####################"
 echo "======================================="
-wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+
+
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
 sudo apt update && sudo apt install terraform -y
+
 terraform --version
+
 echo "======================================="
 echo "#################### Terraform Installed ####################"
 echo "======================================="
+
 sudo rm -rfv terraform.sh
-eho "file removed terraform.sh"
+
+echo "file removed terraform.sh"
