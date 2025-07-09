f71e9015233de75f092478382549676974856309
addico786 <adnandelhi2004@gmail.com>
2025-07-09 11:23:01 +0000

    initial commit

--- docker.sh
+++ docker.sh
@@ -1,4 +1,9 @@
+BLUE_TEXT=$'\033[0;94m'
+GREEN_TEXT=$'\033[0;92m'
+
 sudo apt-get update
 sudo apt-get install docker.io -y
-echo "######################################################## now restart your terminal and then run docker ps ########################################################"
+echo "${GREEN_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT} Docker installed Now restart your terminal and run docker ps  ${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}=======================================${RESET_FORMAT}"
 sudo usermod -aG docker $USER && newgrp
